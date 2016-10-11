4ef00bf9035b3e52f65ceea779386d1ff28c482d
ajcockrum96 <acockru@purdue.edu>
2016-10-10 22:37:55 -0400

    Create helper function that creates a polynomial function using variadic arguments

--- graph.c
+++ graph.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <math.h>
+
+/* Function Prototypes */
+void print_iPoint(struct iPoint* point);
+void print_dPoint(struct dPoint* point);
+void print_graph(int** graph, struct iPoint* range);
+int** _blank_graph(struct iPoint* range);
+void _draw_axes(int** graph, struct iPoint* range, struct iPoint* min, struct dPoint* delta);
+void _poly_graph(int** graph, struct iPoint* range, struct iPoint* min, struct dPoint* delta, int order, ...);
 
 struct iPoint {
 	int x;
@@ -11,63 +20,6 @@ struct dPoint {
 	double x;
 	double y;
 };
-
-void print_iPoint(struct iPoint* point) {
-	printf("(%d, %d)\n", point -> x, point -> y);
-}
-
-void print_dPoint(struct dPoint* point) {
-	printf("(%.2lf, %.2lf)\n", point->x, point->y);
-}
-
-void print_graph(int** graph, struct iPoint* range) {
-	for (int i = range -> y; i >= 0; --i) {
-		for (int j = 0; j < (range -> x + 1); ++j) {
-			switch (graph[i][j]) {
-				case(0):
-					printf(" ");
-					break;
-				case(1):
-					printf("+");
-					break;
-				case(2):
-					printf("-");
-					break;
-				case(3):
-					printf("|");
-					break;
-			}
-		}
-		printf("\n");
-	}
-}
-
-int** _blank_graph(struct iPoint* range) {
-	int** graph = malloc(sizeof(*graph) * (range->y + 1));
-	for (int i = 0; i < (range->y + 1); ++i) {
-		graph[i] = malloc(sizeof(*graph[i]) * (range->x + 1));
-		for (int j = 0; j < (range->x + 1); ++j) {
-			graph[i][j] = 0;
-		}
-	}
-	return graph;
-}
-
-void _draw_axes(int** graph, struct iPoint* range, struct iPoint* min, struct dPoint* delta) {
-	for (int i = 0; i < (range->y + 1); ++i) {
-		for (int j = 0; j < (range->x + 1); ++j) {
-			if ((i == (int)(-round(((min->y) / (delta->y))))) && (j == (int)(-round(((min->x) / (delta->x)))))) {
-				graph[i][j] = 1;
-			}
-			else if (i == (int)(-round(((min->y) / (delta->y))))) {
-				graph[i][j] = 2;
-			}
-			else if (j == (int)(-round(((min->x) / (delta->x))))) {
-				graph[i][j] = 3;
-			}
-		}
-	}
-}
 
 int main(int argc, char* argv[]) {
 	if (argc <= 6) {
@@ -113,6 +65,8 @@ int main(int argc, char* argv[]) {
 		}
 	}
 
+	_poly_graph(graph, range, min, delta, 1, 1, 0);
+
 	print_graph(graph, range);
 	free(min);
 	free(max);
@@ -120,4 +74,78 @@ int main(int argc, char* argv[]) {
 	free(delta);
 	free(graph);
 	return EXIT_SUCCESS;
+}
+
+void print_iPoint(struct iPoint* point) {
+	printf("(%d, %d)\n", point->x, point->y);
+}
+
+void print_dPoint(struct dPoint* point) {
+	printf("(%.2lf, %.2lf)\n", point->x, point->y);
+}
+
+void print_graph(int** graph, struct iPoint* range) {
+	for (int i = range->y; i >= 0; --i) {
+		for (int j = 0; j < (range->x + 1); ++j) {
+			switch (graph[i][j]) {
+			case(0):
+				printf(" ");
+				break;
+			case(1):
+				printf("+");
+				break;
+			case(2):
+				printf("-");
+				break;
+			case(3):
+				printf("|");
+				break;
+			}
+		}
+		printf("\n");
+	}
+}
+
+int** _blank_graph(struct iPoint* range) {
+	int** graph = malloc(sizeof(*graph) * (range->y + 1));
+	for (int i = 0; i < (range->y + 1); ++i) {
+		graph[i] = malloc(sizeof(*graph[i]) * (range->x + 1));
+		for (int j = 0; j < (range->x + 1); ++j) {
+			graph[i][j] = 0;
+		}
+	}
+	return graph;
+}
+
+void _draw_axes(int** graph, struct iPoint* range, struct iPoint* min, struct dPoint* delta) {
+	for (int i = 0; i < (range->y + 1); ++i) {
+		for (int j = 0; j < (range->x + 1); ++j) {
+			if ((i == (int)(-round(((min->y) / (delta->y))))) && (j == (int)(-round(((min->x) / (delta->x)))))) {
+				graph[i][j] = 1;
+			}
+			else if (i == (int)(-round(((min->y) / (delta->y))))) {
+				graph[i][j] = 2;
+			}
+			else if (j == (int)(-round(((min->x) / (delta->x))))) {
+				graph[i][j] = 3;
+			}
+		}
+	}
+}
+
+void _poly_graph(int** graph, struct iPoint* range, struct iPoint* min, struct dPoint* delta, int order, ...) {
+	va_list args;
+	va_start(args, order);
+
+	for (int j = 0; j < (range->x + 1); ++j) {
+		double x = ((double)j * delta->x) + min->x;
+		double y = 0;
+		for (int p = order; p >= 0; ++p) {
+			y += va_arg(args, double) * pow(x, p);
+		}
+		int i = (int)((y - min->y) / delta->y);
+		if (i >= 0 && i < (range->y + 1)) {
+			graph[i][j] = 1;
+		}
+	}
 }
