878508eb78deed6139202a7fc9f9466e56d15563
ajcockrum96 <acockru@purdue.edu>
2016-10-10 22:00:38 -0400

    Create decimal point structure to store scale (delta) values; add corresponding print point function

--- graph.c
+++ graph.c
@@ -6,8 +6,17 @@ struct iPoint {
 	int y;
 };
 
+struct dPoint {
+	double x;
+	double y;
+};
+
 void print_iPoint(struct iPoint* point) {
 	printf("(%d, %d)\n", point -> x, point -> y);
+}
+
+void print_dPoint(struct dPoint* point) {
+	printf("(%.2lf, %.2lf)\n", point->x, point->y);
 }
 
 void print_graph(int** graph, struct iPoint* range) {
@@ -60,22 +69,28 @@ void _draw_axes(int** graph, struct iPoint* range, struct iPoint* min) {
 }
 
 int main(int argc, char* argv[]) {
-	if (argc <= 4) {
+	if (argc <= 6) {
 		printf("Error.  Too few arguments\n");
 		return EXIT_FAILURE;
 	}
 	struct iPoint* min   = malloc(sizeof(*min));
 	struct iPoint* max   = malloc(sizeof(*max));
 	struct iPoint* range = malloc(sizeof(*range));
-	sscanf(argv[1], "%d", &(min -> x));
-	sscanf(argv[2], "%d", &(min -> y));
-	sscanf(argv[3], "%d", &(max -> x));
-	sscanf(argv[4], "%d", &(max -> y));
+	struct dPoint* delta = malloc(sizeof(*delta));
+	sscanf(argv[1], "%d",  &(min->x));
+	sscanf(argv[2], "%d",  &(min->y));
+	sscanf(argv[3], "%d",  &(max->x));
+	sscanf(argv[4], "%d",  &(max->y));
+	sscanf(argv[5], "%lf", &(delta->y));
+	sscanf(argv[6], "%lf", &(delta->y));
+
 	range -> x = (max -> x) - (min -> x);
 	range -> y = (max -> y) - (min -> y);
+
 	print_iPoint(min);
 	print_iPoint(max);
 	print_iPoint(range);
+	print_dPoint(delta);
 	if (range -> x < 0 || range -> y < 0) {
 		printf("Error, silly arguments.\n");
 		free(min);
@@ -91,6 +106,7 @@ int main(int argc, char* argv[]) {
 	free(min);
 	free(max);
 	free(range);
+	free(delta);
 	free(graph);
 	return EXIT_SUCCESS;
 }
