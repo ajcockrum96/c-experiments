9f2a9f26fa6c8142448960fcc4ecc63110bdea2a
ajcockrum96 <acockru@purdue.edu>
2016-10-11 22:12:33 -0400

    Instruct function to add values to schedule at appropriate times; add recursive call to function (because number of loops needed is unknown); create null/zero test call

--- ScheduleGenerator/schedule.cpp
+++ ScheduleGenerator/schedule.cpp
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-void generate_schedule(int numClasses, int* numOptions, int** classTimeArrays, int** schedule);
+void generate_schedule(int numClasses, int* numOptions, int*** classTimeArrays, int** schedule);
 int** blank_schedule();
 
 int main(int argc, char* argv[]) {
+	int** schedule = blank_schedule();
+	generate_schedule(0, NULL, NULL, schedule);
+	free(schedule);
 	return EXIT_SUCCESS;
 }
 
@@ -22,6 +26,28 @@ int** blank_schedule() {
 	return schedule;
 }
 
-void generate_schedule(int numClasses, int* numOptions, int** classTimeArrays, int** schedule) {
-
+void generate_schedule(int numClasses, int* numOptions, int*** classTimeArrays, int** schedule) {
+	if ((numClasses > 0 && numOptions != NULL) && (classTimeArrays != NULL && schedule != NULL)) {
+		for (int i = 0; i < numOptions[numClasses - 1]; ++i) {
+			for (int j = 0; j < numDays; ++j) {
+				int start = classTimeArrays[numOptions - 1][j][i];
+				int end = start + classLengthArrays[numOptions - 1][j][i];
+				bool abort = false;
+				for (int k = start; k < end; ++k) {
+					if (schedule[j][k] == 0) {
+						abort = true;
+						break;
+					}
+				}
+				if (!abort) {
+					for (int k = start; k < end; ++k) {
+						schedule[j][k] = numClasses - 1;
+					}
+				}
+				if (numClasses > 1) {
+					generate_schedule(numClasses - 1, numOptions, classTimeArrays, schedule);
+				}
+			}
+		}
+	}
 }
