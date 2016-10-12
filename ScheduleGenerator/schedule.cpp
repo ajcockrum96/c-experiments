a5894551a52f4958dd3846ee9882f03f187d9dc9
ajcockrum96 <acockru@purdue.edu>
2016-10-11 22:34:59 -0400

    Remove recursive call and simplify generate function to start

--- ScheduleGenerator/schedule.cpp
+++ ScheduleGenerator/schedule.cpp
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void generate_schedule(int numClasses, int numDays, int* numOptions, int*** classTimeArrays, int*** classLengthArrays, int** schedule);
+void generate_schedule(int numClasses, int numDays, int numHours, int periodsPerHour, int* numOptions, int*** classTimeArrays, int*** classLengthArrays, int** schedule);
 int** blank_schedule(int numDays, int numHours, int periodsPerHour);
 void free_schedule(int** schedule, int numDays);
 void print_schedule(int** schedule, int numDays, int numHours, int periodsPerHour);
@@ -12,7 +12,19 @@ int main(int argc, char* argv[]) {
 	int numHours = 10;
 	int periodsPerHour = 2;
 	int** schedule = blank_schedule(numDays, numHours, periodsPerHour);
-	generate_schedule(0, numDays, NULL, NULL, NULL, schedule);
+	int numOptions[] = {
+		1
+	};
+	int classTimeArrays[][] = {
+		{
+			{1},
+			{0},
+			{1},
+			{0},
+			{1}
+		}
+	};
+	generate_schedule(0, numDays, numHours, periodsPerHour, NULL, NULL, NULL, schedule);
 	print_schedule(schedule, numDays, numHours, periodsPerHour);
 	free(schedule);
 	return EXIT_SUCCESS;
@@ -45,26 +57,22 @@ void print_schedule(int** schedule, int numDays, int numHours, int periodsPerHou
 	}
 }
 
-void generate_schedule(int numClasses, int numDays, int* numOptions, int*** classTimeArrays, int*** classLengthArrays, int** schedule) {
+void generate_schedule(int numClasses, int numDays, int numHours, int periodsPerHour, int* numOptions, int*** classTimeArrays, int*** classLengthArrays, int** schedule) {
 	if ((numClasses > 0 && numOptions != NULL) && (classTimeArrays != NULL && schedule != NULL)) {
 		for (int i = 0; i < numOptions[numClasses - 1]; ++i) {
 			for (int j = 0; j < numDays; ++j) {
 				int start = classTimeArrays[numClasses - 1][j][i];
 				int end = start + classLengthArrays[numClasses - 1][j][i];
-				bool abort = false;
 				for (int k = start; k < end; ++k) {
-					if (schedule[j][k] == 0) {
-						abort = true;
-						break;
-					}
+					schedule[j][k] = numClasses - 1;
 				}
-				if (!abort) {
-					for (int k = start; k < end; ++k) {
-						schedule[j][k] = numClasses - 1;
-					}
-				}
-				if (numClasses > 1) {
-					generate_schedule(numClasses - 1, numDays, numOptions, classTimeArrays, classLengthArrays, schedule);
+			}
+			print_schedule(schedule, numDays, numHours, periodsPerHour);
+			for (int j = 0; j < numDays; ++j) {
+				int start = classTimeArrays[numClasses - 1][j][i];
+				int end = start + classLengthArrays[numClasses - 1][j][i];
+				for (int k = start; k < end; ++k) {
+					schedule[j][k] = 0;
 				}
 			}
 		}
