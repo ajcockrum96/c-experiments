70da2f34632d1ebd57f0f4588d5d7359cfa3b98b
ajcockrum96 <acockru@purdue.edu>
2016-10-11 22:20:49 -0400

    Fix compiler errors; add arguments to blank_schedule for versatility; add function free_schedule to properly free allocated memory

--- ScheduleGenerator/schedule.cpp
+++ ScheduleGenerator/schedule.cpp
@@ -2,23 +2,24 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-void generate_schedule(int numClasses, int* numOptions, int*** classTimeArrays, int** schedule);
-int** blank_schedule();
+void generate_schedule(int numClasses, int numDays, int* numOptions, int*** classTimeArrays, int*** classLengthArrays, int** schedule);
+int** blank_schedule(int numDays);
+void free_schedule(int** schedule, int numDays);
 
 int main(int argc, char* argv[]) {
-	int** schedule = blank_schedule();
-	generate_schedule(0, NULL, NULL, schedule);
+	int numDays = 5;
+	int** schedule = blank_schedule(numDays);
+	generate_schedule(0, numDays, NULL, NULL, NULL, schedule);
 	free(schedule);
 	return EXIT_SUCCESS;
 }
 
-int** blank_schedule() {
-	int numDays = 5;
+int** blank_schedule(int numDays) {
 	int numHours = 10;
 	int periodsPerHour = 2;
-	int** schedule = malloc(sizeof(*schedule) * numDays));
+	int** schedule = (int**)malloc(sizeof(*schedule) * numDays);
 	for (int i = 0; i < numDays; ++i) {
-		schedule[i] = malloc(sizeof(*schedule[i]) * numHours * periodsPerHour);
+		schedule[i] = (int*)malloc(sizeof(*schedule[i]) * numHours * periodsPerHour);
 		for (int j = 0; j < numHours * periodsPerHour; ++j) {
 			schedule[i][j] = 0;
 		}
@@ -26,12 +27,19 @@ int** blank_schedule() {
 	return schedule;
 }
 
-void generate_schedule(int numClasses, int* numOptions, int*** classTimeArrays, int** schedule) {
+void free_schedule(int** schedule, int numDays) {
+	for (int i = 0; i < numDays; ++i) {
+		free(schedule[i]);
+	}
+	free(schedule);
+}
+
+void generate_schedule(int numClasses, int numDays, int* numOptions, int*** classTimeArrays, int*** classLengthArrays, int** schedule) {
 	if ((numClasses > 0 && numOptions != NULL) && (classTimeArrays != NULL && schedule != NULL)) {
 		for (int i = 0; i < numOptions[numClasses - 1]; ++i) {
 			for (int j = 0; j < numDays; ++j) {
-				int start = classTimeArrays[numOptions - 1][j][i];
-				int end = start + classLengthArrays[numOptions - 1][j][i];
+				int start = classTimeArrays[numClasses - 1][j][i];
+				int end = start + classLengthArrays[numClasses - 1][j][i];
 				bool abort = false;
 				for (int k = start; k < end; ++k) {
 					if (schedule[j][k] == 0) {
@@ -45,7 +53,7 @@ void generate_schedule(int numClasses, int* numOptions, int*** classTimeArrays, 
 					}
 				}
 				if (numClasses > 1) {
-					generate_schedule(numClasses - 1, numOptions, classTimeArrays, schedule);
+					generate_schedule(numClasses - 1, numDays, numOptions, classTimeArrays, classLengthArrays, schedule);
 				}
 			}
 		}
