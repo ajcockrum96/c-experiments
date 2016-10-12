a562b48c9ebf0e903b27ef4e3f67623f82fb4648
ajcockrum96 <acockru@purdue.edu>
2016-10-11 21:52:47 -0400

    Create Function Definition for schedule generator

---
+++ ScheduleGenerator/schedule.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void generate_schedule(int numClasses, int* numOptions, int** classTimeArrays);
+
+int main(int argc, char* argv[]) {
+	return EXIT_SUCCESS;
+}
