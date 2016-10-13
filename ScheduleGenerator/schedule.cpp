ed07ba4fb49f2ef1704c5cdec001c887a423592e
ajcockrum96 <acockru@purdue.edu>
2016-10-12 23:07:49 -0400

    Read in lines of input file; allocate space based on expected format of input file; print lines for test case; create count lines function for proper memory allocation; create round_to function for later usage on time handling

--- ScheduleGenerator/schedule.cpp
+++ ScheduleGenerator/schedule.cpp
@@ -22,18 +22,13 @@ int main(int argc, const char* argv[]) {
 		printf("Error, could not open input file.\n");
 		return EXIT_FAILURE;
 	}
-	int numClasses = 6;
+	int numClasses = 1;
 	int numDays = 5;
 	int numHours = 10;
 	int periodsPerHour = 2;
 	int** schedule = blank_schedule(numDays, numHours, periodsPerHour);
 	int numOptions[] = {
-		6,
-		13,
-		1,
-		10,
-		1,
-		2
+		6
 	};
 	int*** classTimeArrays = (int***)malloc(sizeof(*classTimeArrays) * numClasses);
 	for (int i = 0; i < numClasses; ++i) {
@@ -42,174 +37,6 @@ int main(int argc, const char* argv[]) {
 			classTimeArrays[i][j] = (int*)malloc(sizeof(*classTimeArrays[i][j]) * numOptions[i]);
 		}
 	}
-	/* ECE 20200 */
-	classTimeArrays[5][0][0] = 0;
-	classTimeArrays[5][1][0] = 0;
-	classTimeArrays[5][2][0] = 0;
-	classTimeArrays[5][3][0] = 0;
-	classTimeArrays[5][4][0] = 0;
-
-	classTimeArrays[5][0][1] = 0;
-	classTimeArrays[5][1][1] = 12;
-	classTimeArrays[5][2][1] = 0;
-	classTimeArrays[5][3][1] = 12;
-	classTimeArrays[5][4][1] = 0;
-
-	/* ECE 270 */
-	classTimeArrays[4][0][0] = 18;
-	classTimeArrays[4][1][0] = 0;
-	classTimeArrays[4][2][0] = 18;
-	classTimeArrays[4][3][0] = 0;
-	classTimeArrays[4][4][0] = 18;
-
-	classTimeArrays[3][0][0] = 0;
-	classTimeArrays[3][1][0] = 0;
-	classTimeArrays[3][2][0] = 0;
-	classTimeArrays[3][3][0] = 0;
-	classTimeArrays[3][4][0] = 0;
-
-	classTimeArrays[3][0][1] = 0;
-	classTimeArrays[3][1][1] = 8;
-	classTimeArrays[3][2][1] = 0;
-	classTimeArrays[3][3][1] = 0;
-	classTimeArrays[3][4][1] = 0;
-
-	classTimeArrays[3][0][2] = 0;
-	classTimeArrays[3][1][2] = 14;
-	classTimeArrays[3][2][2] = 0;
-	classTimeArrays[3][3][2] = 0;
-	classTimeArrays[3][4][2] = 0;
-
-	classTimeArrays[3][0][3] = 0;
-	classTimeArrays[3][1][3] = 0;
-	classTimeArrays[3][2][3] = 0;
-	classTimeArrays[3][3][3] = 0;
-	classTimeArrays[3][4][3] = 0;
-
-	classTimeArrays[3][0][4] = 0;
-	classTimeArrays[3][1][4] = 0;
-	classTimeArrays[3][2][4] = 6;
-	classTimeArrays[3][3][4] = 0;
-	classTimeArrays[3][4][4] = 0;
-
-	classTimeArrays[3][0][5] = 0;
-	classTimeArrays[3][1][5] = 0;
-	classTimeArrays[3][2][5] = 12;
-	classTimeArrays[3][3][5] = 0;
-	classTimeArrays[3][4][5] = 0;
-
-	classTimeArrays[3][0][6] = 0;
-	classTimeArrays[3][1][6] = 0;
-	classTimeArrays[3][2][6] = 0;
-	classTimeArrays[3][3][6] = 0;
-	classTimeArrays[3][4][6] = 0;
-
-	classTimeArrays[3][0][7] = 0;
-	classTimeArrays[3][1][7] = 0;
-	classTimeArrays[3][2][7] = 0;
-	classTimeArrays[3][3][7] = 8;
-	classTimeArrays[3][4][7] = 0;
-
-	classTimeArrays[3][0][8] = 0;
-	classTimeArrays[3][1][8] = 0;
-	classTimeArrays[3][2][8] = 0;
-	classTimeArrays[3][3][8] = 0;
-	classTimeArrays[3][4][8] = 6;
-
-	classTimeArrays[3][0][9] = 0;
-	classTimeArrays[3][1][9] = 0;
-	classTimeArrays[3][2][9] = 0;
-	classTimeArrays[3][3][9] = 0;
-	classTimeArrays[3][4][9] = 12;
-
-	/* ECE 36800 */
-	classTimeArrays[2][0][0] = 0;
-	classTimeArrays[2][1][0] = 15;
-	classTimeArrays[2][2][0] = 0;
-	classTimeArrays[2][3][0] = 15;
-	classTimeArrays[2][4][0] = 0;
-
-	/* MA 26600 */
-	classTimeArrays[1][0][0] = 2;
-	classTimeArrays[1][1][0] = 0;
-	classTimeArrays[1][2][0] = 2;
-	classTimeArrays[1][3][0] = 0;
-	classTimeArrays[1][4][0] = 2;
-
-	classTimeArrays[1][0][1] = 4;
-	classTimeArrays[1][1][1] = 0;
-	classTimeArrays[1][2][1] = 4;
-	classTimeArrays[1][3][1] = 0;
-	classTimeArrays[1][4][1] = 4;
-
-	classTimeArrays[1][0][2] = 6;
-	classTimeArrays[1][1][2] = 0;
-	classTimeArrays[1][2][2] = 6;
-	classTimeArrays[1][3][2] = 0;
-	classTimeArrays[1][4][2] = 6;
-
-	classTimeArrays[1][0][3] = 8;
-	classTimeArrays[1][1][3] = 0;
-	classTimeArrays[1][2][3] = 8;
-	classTimeArrays[1][3][3] = 0;
-	classTimeArrays[1][4][3] = 8;
-
-	classTimeArrays[1][0][4] = 10;
-	classTimeArrays[1][1][4] = 0;
-	classTimeArrays[1][2][4] = 10;
-	classTimeArrays[1][3][4] = 0;
-	classTimeArrays[1][4][4] = 10;
-
-	classTimeArrays[1][0][5] = 12;
-	classTimeArrays[1][1][5] = 0;
-	classTimeArrays[1][2][5] = 12;
-	classTimeArrays[1][3][5] = 0;
-	classTimeArrays[1][4][5] = 12;
-
-	classTimeArrays[1][0][6] = 14;
-	classTimeArrays[1][1][6] = 0;
-	classTimeArrays[1][2][6] = 14;
-	classTimeArrays[1][3][6] = 0;
-	classTimeArrays[1][4][6] = 14;
-	
-	classTimeArrays[1][0][7] = 16;
-	classTimeArrays[1][1][7] = 0;
-	classTimeArrays[1][2][7] = 16;
-	classTimeArrays[1][3][7] = 0;
-	classTimeArrays[1][4][7] = 16;
-
-	classTimeArrays[1][0][8] = 0;
-	classTimeArrays[1][1][8] = 6;
-	classTimeArrays[1][2][8] = 0;
-	classTimeArrays[1][3][8] = 6;
-	classTimeArrays[1][4][8] = 0;
-
-	classTimeArrays[1][0][9] = 0;
-	classTimeArrays[1][1][9] = 9;
-	classTimeArrays[1][2][9] = 0;
-	classTimeArrays[1][3][9] = 9;
-	classTimeArrays[1][4][9] = 0;
-
-	classTimeArrays[1][0][10] = 0;
-	classTimeArrays[1][1][10] = 12;
-	classTimeArrays[1][2][10] = 0;
-	classTimeArrays[1][3][10] = 12;
-	classTimeArrays[1][4][10] = 0;
-
-	classTimeArrays[1][0][11] = 0;
-	classTimeArrays[1][1][11] = 15;
-	classTimeArrays[1][2][11] = 0;
-	classTimeArrays[1][3][11] = 15;
-	classTimeArrays[1][4][11] = 0;
-
-	classTimeArrays[1][0][12] = 0;
-	classTimeArrays[1][1][12] = 18;
-	classTimeArrays[1][2][12] = 0;
-	classTimeArrays[1][3][12] = 18;
-	classTimeArrays[1][4][12] = 0;
-
-	/* MA 35100 */
-	/* In file */
 
 	int*** classLengthArrays = (int***)malloc(sizeof(*classLengthArrays) * numClasses);
 	for (int i = 0; i < numClasses; ++i) {
@@ -218,210 +45,9 @@ int main(int argc, const char* argv[]) {
 			classLengthArrays[i][j] = (int*)malloc(sizeof(*classLengthArrays[i][j]) * numOptions[i]);
 		}
 	}
-	/* ECE 20200 */
-	classLengthArrays[5][0][0] = 2;
-	classLengthArrays[5][1][0] = 0;
-	classLengthArrays[5][2][0] = 2;
-	classLengthArrays[5][3][0] = 0;
-	classLengthArrays[5][4][0] = 2;
 
-	classLengthArrays[5][0][1] = 0;
-	classLengthArrays[5][1][1] = 3;
-	classLengthArrays[5][2][1] = 0;
-	classLengthArrays[5][3][1] = 3;
-	classLengthArrays[5][4][1] = 0;
-
-	/* ECE 270 */
-	classLengthArrays[4][0][0] = 2;
-	classLengthArrays[4][1][0] = 0;
-	classLengthArrays[4][2][0] = 2;
-	classLengthArrays[4][3][0] = 0;
-	classLengthArrays[4][4][0] = 2;
-
-	classLengthArrays[3][0][0] = 0;
-	classLengthArrays[3][1][0] = 6;
-	classLengthArrays[3][2][0] = 0;
-	classLengthArrays[3][3][0] = 0;
-	classLengthArrays[3][4][0] = 0;
-
-	classLengthArrays[3][0][1] = 0;
-	classLengthArrays[3][1][1] = 6;
-	classLengthArrays[3][2][1] = 0;
-	classLengthArrays[3][3][1] = 0;
-	classLengthArrays[3][4][1] = 0;
-
-	classLengthArrays[3][0][2] = 0;
-	classLengthArrays[3][1][2] = 6;
-	classLengthArrays[3][2][2] = 0;
-	classLengthArrays[3][3][2] = 0;
-	classLengthArrays[3][4][2] = 0;
-
-	classLengthArrays[3][0][3] = 0;
-	classLengthArrays[3][1][3] = 0;
-	classLengthArrays[3][2][3] = 6;
-	classLengthArrays[3][3][3] = 0;
-	classLengthArrays[3][4][3] = 0;
-
-	classLengthArrays[3][0][4] = 0;
-	classLengthArrays[3][1][4] = 0;
-	classLengthArrays[3][2][4] = 6;
-	classLengthArrays[3][3][4] = 0;
-	classLengthArrays[3][4][4] = 0;
-
-	classLengthArrays[3][0][5] = 0;
-	classLengthArrays[3][1][5] = 0;
-	classLengthArrays[3][2][5] = 6;
-	classLengthArrays[3][3][5] = 0;
-	classLengthArrays[3][4][5] = 0;
-
-	classLengthArrays[3][0][6] = 0;
-	classLengthArrays[3][1][6] = 0;
-	classLengthArrays[3][2][6] = 0;
-	classLengthArrays[3][3][6] = 6;
-	classLengthArrays[3][4][6] = 0;
-
-	classLengthArrays[3][0][7] = 0;
-	classLengthArrays[3][1][7] = 0;
-	classLengthArrays[3][2][7] = 0;
-	classLengthArrays[3][3][7] = 6;
-	classLengthArrays[3][4][7] = 0;
-
-	classLengthArrays[3][0][8] = 0;
-	classLengthArrays[3][1][8] = 0;
-	classLengthArrays[3][2][8] = 0;
-	classLengthArrays[3][3][8] = 0;
-	classLengthArrays[3][4][8] = 6;
-
-	classLengthArrays[3][0][9] = 0;
-	classLengthArrays[3][1][9] = 0;
-	classLengthArrays[3][2][9] = 0;
-	classLengthArrays[3][3][9] = 0;
-	classLengthArrays[3][4][9] = 6;
-
-	/* ECE 36800 */
-	classLengthArrays[2][0][0] = 0;
-	classLengthArrays[2][1][0] = 3;
-	classLengthArrays[2][2][0] = 0;
-	classLengthArrays[2][3][0] = 3;
-	classLengthArrays[2][4][0] = 0;
-
-	/* MA 26600 */
-	classLengthArrays[1][0][0] = 2;
-	classLengthArrays[1][1][0] = 0;
-	classLengthArrays[1][2][0] = 2;
-	classLengthArrays[1][3][0] = 0;
-	classLengthArrays[1][4][0] = 2;
-
-	classLengthArrays[1][0][1] = 2;
-	classLengthArrays[1][1][1] = 0;
-	classLengthArrays[1][2][1] = 2;
-	classLengthArrays[1][3][1] = 0;
-	classLengthArrays[1][4][1] = 2;
-
-	classLengthArrays[1][0][2] = 2;
-	classLengthArrays[1][1][2] = 0;
-	classLengthArrays[1][2][2] = 2;
-	classLengthArrays[1][3][2] = 0;
-	classLengthArrays[1][4][2] = 2;
-
-	classLengthArrays[1][0][3] = 2;
-	classLengthArrays[1][1][3] = 0;
-	classLengthArrays[1][2][3] = 2;
-	classLengthArrays[1][3][3] = 0;
-	classLengthArrays[1][4][3] = 2;
-
-	classLengthArrays[1][0][4] = 2;
-	classLengthArrays[1][1][4] = 0;
-	classLengthArrays[1][2][4] = 2;
-	classLengthArrays[1][3][4] = 0;
-	classLengthArrays[1][4][4] = 2;
-
-	classLengthArrays[1][0][5] = 2;
-	classLengthArrays[1][1][5] = 0;
-	classLengthArrays[1][2][5] = 2;
-	classLengthArrays[1][3][5] = 0;
-	classLengthArrays[1][4][5] = 2;
-
-	classLengthArrays[1][0][6] = 2;
-	classLengthArrays[1][1][6] = 0;
-	classLengthArrays[1][2][6] = 2;
-	classLengthArrays[1][3][6] = 0;
-	classLengthArrays[1][4][6] = 2;
-
-	classLengthArrays[1][0][7] = 2;
-	classLengthArrays[1][1][7] = 0;
-	classLengthArrays[1][2][7] = 2;
-	classLengthArrays[1][3][7] = 0;
-	classLengthArrays[1][4][7] = 2;
-
-	classLengthArrays[1][0][8] = 0;
-	classLengthArrays[1][1][8] = 3;
-	classLengthArrays[1][2][8] = 0;
-	classLengthArrays[1][3][8] = 3;
-	classLengthArrays[1][4][8] = 0;
-
-	classLengthArrays[1][0][9] = 0;
-	classLengthArrays[1][1][9] = 3;
-	classLengthArrays[1][2][9] = 0;
-	classLengthArrays[1][3][9] = 3;
-	classLengthArrays[1][4][9] = 0;
-
-	classLengthArrays[1][0][10] = 0;
-	classLengthArrays[1][1][10] = 3;
-	classLengthArrays[1][2][10] = 0;
-	classLengthArrays[1][3][10] = 3;
-	classLengthArrays[1][4][10] = 0;
-
-	classLengthArrays[1][0][11] = 0;
-	classLengthArrays[1][1][11] = 3;
-	classLengthArrays[1][2][11] = 0;
-	classLengthArrays[1][3][11] = 3;
-	classLengthArrays[1][4][11] = 0;
-
-	classLengthArrays[1][0][12] = 0;
-	classLengthArrays[1][1][12] = 2;
-	classLengthArrays[1][2][12] = 0;
-	classLengthArrays[1][3][12] = 2;
-	classLengthArrays[1][4][12] = 0;
-
-	/* MA 35100 */
-	classLengthArrays[0][0][0] = 2;
-	classLengthArrays[0][1][0] = 0;
-	classLengthArrays[0][2][0] = 2;
-	classLengthArrays[0][3][0] = 0;
-	classLengthArrays[0][4][0] = 2;
-
-	classLengthArrays[0][0][1] = 2;
-	classLengthArrays[0][1][1] = 0;
-	classLengthArrays[0][2][1] = 2;
-	classLengthArrays[0][3][1] = 0;
-	classLengthArrays[0][4][1] = 2;
-
-	classLengthArrays[0][0][2] = 2;
-	classLengthArrays[0][1][2] = 0;
-	classLengthArrays[0][2][2] = 2;
-	classLengthArrays[0][3][2] = 0;
-	classLengthArrays[0][4][2] = 2;
-
-	classLengthArrays[0][0][3] = 0;
-	classLengthArrays[0][1][3] = 3;
-	classLengthArrays[0][2][3] = 0;
-	classLengthArrays[0][3][3] = 3;
-	classLengthArrays[0][4][3] = 0;
-
-	classLengthArrays[0][0][4] = 0;
-	classLengthArrays[0][1][4] = 3;
-	classLengthArrays[0][2][4] = 0;
-	classLengthArrays[0][3][4] = 3;
-	classLengthArrays[0][4][4] = 0;
-
-	classLengthArrays[0][0][5] = 0;
-	classLengthArrays[0][1][5] = 3;
-	classLengthArrays[0][2][5] = 0;
-	classLengthArrays[0][3][5] = 3;
-	classLengthArrays[0][4][5] = 0;
-
-	generate_schedule(numClasses, numDays, numHours, periodsPerHour, numOptions, classTimeArrays, classLengthArrays, schedule);
+	read_input(input, NULL, NULL, 1, 5, 'M', 10, 8, 30, 2);
+	// generate_schedule(numClasses, numDays, numHours, periodsPerHour, numOptions, classTimeArrays, classLengthArrays, schedule);
 	free(schedule);
 	free_class_times(classTimeArrays, numClasses, numDays);
 	free_class_times(classLengthArrays, numClasses, numDays);
@@ -518,5 +144,38 @@ void generate_schedule(int numClasses, int numDays, int numHours, int periodsPer
 	}
 }
 
-void read_input(FILE* input, int*** classTimeArrays, int*** classLength Arrays, int numClasses, int numDays, char firstDay, int numHours, int firstHour, int firstMin, int periodsPerHour) {
+void read_input(FILE* input, int*** classTimeArrays, int*** classLengthArrays, int numClasses, int numDays, char firstDay, int numHours, int firstHour, int firstMin, int periodsPerHour) {
+	int numLines = _count_lines(input);
+	int lineLen = 8 + 5 + 1 + 5;
+	char** lines = malloc(sizeof(*lines) * numLines);
+	for (int i = 0; i < numLines; ++i) {
+		lines[i] = malloc(sizeof(*lines[i]) * lineLen);
+		fgets(lines[i], lineLen, input);
+		printf("%s", lines[i]);
+	}
+}
+
+int _count_lines(FILE* file) {
+	int count = 0;
+	int pos = SEEK_CUR;
+	fseek(file, 0, SEEK_SET);
+	while (!feof(file)) {
+		char temp = fgetc(file);
+		if (temp == '\n') {
+			++count;
+		}
+	}
+	fseek(file, 0, pos);
+	return count;
+}
+
+int _round_to(int value, int round) {
+	int lower = (value % round) * round;
+	int upper = lower + round;
+	if (abs(lower - value) < abs(upper - value)) {
+		return lower;
+	}
+	else {
+		return upper;
+	}
 }
